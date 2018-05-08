0bd7dfa1859b6d46afaee4ca8a7abb2bbc5c6c8e
郏要 <jiayao@meituan.com>
2018-05-08 17:19:39 +0800

    Initial Commit

---
+++ DesignPatterns_For_iOS_OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DesignPatterns_For_iOS_OC
+//
+//  Created by Dely on 2018/5/8.
+//  Copyright © 2018年 meituan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
